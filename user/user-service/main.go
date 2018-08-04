94f508e7a0ca0987a62ef4a1ec101a72257223d8
wule <wule@wuledeMBP.lan>
2018-08-04 16:39:29 +0800

    添加注释

--- user/user-service/main.go
+++ user/user-service/main.go
@@ -10,19 +10,21 @@ import (
 )
 
 type Users struct {
+	//作为客户端调用服务端
 	userid pbid.UserIdService
 }
 
 func (u *Users) GetUserInfo(ctx context.Context,req *pb.Request,resp *pb.Response) error {
+
+	//构建数据
 	vReq := &pbid.Request{Name:req.Name}
 
+	//调用服务端函数
 	vResp,err := u.userid.GetUserId(ctx,vReq)
 
 	if err != nil {
 		return err
 	}
-
-
 	resp.Getting = "hello   " + req.Name + strconv.Itoa(int(vResp.Id))
 	return nil
 }
@@ -30,14 +32,20 @@ func (u *Users) GetUserInfo(ctx context.Context,req *pb.Request,resp *pb.Respons
 
 
 func main() {
-
-	service := micro.NewService(micro.Name("user"))
+	//创建一个服务
+	service := micro.NewService(
+		micro.Name("user"),    //
+		)
 
 	service.Init()
 
+	//服务之间通信,使其作为客户端调用服务端,创建一个客户端
 	client := pbid.NewUserIdService("userid", service.Client())
 
+	//注册服务
 	pb.RegisterUsersHandler(service.Server(),&Users{userid:client})
+
+	//运行服务
 	if err := service.Run(); err != nil {
 		fmt.Println(err)
 	}
